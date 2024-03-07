65dc59aa49efe7cae35039efbf6f2d249e4e970f
VyenkteshMohite <mohitevyenktesh@gmail.com>
2024-03-07 08:48:02 +0530

    Header and Footer Component Added

---
+++ src/components/FooterComponenet.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const FooterComponenet = () => {
+  return (
+    <div>
+        <footer className="footer">
+            <span className="text-muted">
+                    All Rights Are Reserved 2024 @TechnoBlogs
+            </span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponenet
---
+++ src/components/HeaderComponent.js
@@ -0,0 +1,21 @@
+//to creat react snippet use rafce means=react arrow functional componenet export
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+        <header>
+            <nav className="navbar navbar-expanad-md navbar-dark bg-dark">
+                <div>
+                    <a href = "https://appliedartificialintelligencejournal.blogspot.com/" className='nvabar-brand'>
+
+                            Employee Management WebApplication
+                    </a>
+                </div>
+            </nav>
+        </header>
+    </div>
+  )
+}
+
+export default HeaderComponent
